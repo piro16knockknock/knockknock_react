64de04145a6e9e3d2ca08ef0c94f8e164b86e7da
suu3 <ahob789@ewhain.net>
2022-04-07 23:47:38 +0900

    Setting

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import "./css/style.css";
+import Header from "./components/header";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
