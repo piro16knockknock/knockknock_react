b51e966cdd073421e457355ac3fe5e51b3398fac
suu3 <ahob789@ewhain.net>
2022-05-28 12:36:15 +0900

    [FEAT] piei chart

--- src/pages/mypage/goal.jsx
+++ src/pages/mypage/goal.jsx
@@ -1,6 +1,7 @@
+import Pie from "components/pie";
 import React from "react";
 import styles from "styles/mypage/myhouse.module.css";
-
+import "styles/colors.css";
 const Goal = (props) => {
   return (
     <section className={styles[`container`]}>
@@ -8,7 +9,24 @@ const Goal = (props) => {
         <h3>달성률</h3>
       </div>
       <ul>
-        <li></li>
+        <li>
+          <Pie
+            percentage={90}
+            width={120}
+            barColor={`#FFC56D`}
+            fontSize={20}
+            barSize={15}
+          />
+        </li>
+        <li>
+          <Pie
+            percentage={100}
+            width={120}
+            barColor={`#89BA9F`}
+            fontSize={20}
+            barSize={15}
+          />
+        </li>
       </ul>
     </section>
   );
