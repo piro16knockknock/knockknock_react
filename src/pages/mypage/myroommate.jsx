a65dc2e3f7d06a9b9012ef8badeabbce174d20e5
suu3 <ahob789@ewhain.net>
2022-05-29 00:48:24 +0900

    [FEAT] 마이페이지 모달

--- src/pages/mypage/myroommate.jsx
+++ src/pages/mypage/myroommate.jsx
@@ -1,22 +1,65 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styles from "styles/mypage/myroommate.module.css";
 import { ICONS } from "lib/assets";
+import { roommateList } from "lib/dummy_roommate";
+import Modal from "components/modal";
+
 const MyRoommate = (props) => {
+  const [modalShow, setModalShow] = useState(false);
+  const [datas, setDatas] = useState([]);
+  const [index, setIndex] = useState(0);
+
+  const modal = useRef();
+  const openBtn = useRef();
+
+  const handleCloseModal = (e) => {
+    if (
+      modal.current &&
+      !modal.current.contains(e.target) &&
+      e.target.className !== styles[`info-edit-btn`]
+    ) {
+      setModalShow(false);
+      return;
+    }
+  };
+
+  useEffect(() => {
+    setDatas(roommateList);
+    window.addEventListener("click", handleCloseModal);
+    return () => {
+      window.removeEventListener("click", handleCloseModal);
+    };
+  }, []);
+
   return (
     <section className={styles[`container`]}>
       <div className={styles[`title`]}>
         <h3>나의 룸메이트</h3> <img src={ICONS.BORDER_PLUS} alt="plus" />
       </div>
       <ul>
-        <li className={styles[`roommate`]}>
-          <span>이세윤</span>
-          <button className={styles[`info-edit-btn`]}>프로필 보기</button>
-        </li>{" "}
-        <li className={styles[`roommate`]}>
-          <span>이세윤</span>
-          <button className={styles[`info-edit-btn`]}>프로필 보기</button>
-        </li>
+        {datas.map((v, i) => {
+          return (
+            <li className={styles[`roommate`]} key={i}>
+              <span>{v.nick_name}</span>
+              <button
+                onClick={() => {
+                  setModalShow(true);
+                  setIndex(i);
+                }}
+                className={styles[`info-edit-btn`]}
+              >
+                프로필 보기
+              </button>
+            </li>
+          );
+        })}
       </ul>
+      <Modal
+        data={datas[index]}
+        ref={modal}
+        show={modalShow}
+        setShow={setModalShow}
+      />
     </section>
   );
 };
