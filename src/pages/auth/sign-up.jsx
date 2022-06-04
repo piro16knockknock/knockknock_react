c903848d96caa38951c5d19b0ce3a4aae7b48e69
suu3 <ahob789@ewhain.net>
2022-06-04 17:04:11 +0900

    [FIX] custom select

--- src/pages/auth/sign-up.jsx
+++ src/pages/auth/sign-up.jsx
@@ -3,13 +3,12 @@ import CustomInput from "components/auth/custom-input";
 import common from "styles/auth/common.module.css";
 import ConfirmBtn from "components/auth/confirm-btn";
 import CustomSelect from "components/auth/custom-select";
-
+import useInput from "hook/useInput";
 const SignUp = (props) => {
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConfirm, setPasswordConfirm] = useState("");
   const [birthday, setBirthday] = useState("");
-  const [gender, setGender] = useState("");
   const [phone, setPhone] = useState("");
 
   const handleId = (e) => {
@@ -27,21 +26,20 @@ const SignUp = (props) => {
     setBirthday(e.target.value);
   };
 
-  const handleGender = (e) => {
-    setGender(e.target.value);
-  };
+  const { value: gender, onChange: handleGender } = useInput();
+
   const handlePhone = (e) => {
     setPhone(e.target.value);
   };
 
   return (
-    <section className={common.container}>
+    <form className={common.container}>
       <p className={common.title}>회원가입</p>
       <CustomInput label="아이디" onChange={handleId} />
       <CustomInput label="비밀번호" onChange={handlePassword} />
       <CustomInput label="비밀번호 재확인" onChange={handlePasswordConfirm} />
       <CustomInput label="생년월일" onChange={handleBirthday} />
-      <CustomSelect label="성별" onChange={handleGender} />
+      <CustomSelect label="성별" value={gender} onChange={handleGender} />
       <CustomInput label="휴대전화" onChange={handlePhone} />
       <ConfirmBtn
         label="가입하기"
@@ -56,7 +54,7 @@ const SignUp = (props) => {
             : false
         }
       />
-    </section>
+    </form>
   );
 };
 
