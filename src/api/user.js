5deab2d2b0e111a20134f02db2101465297f37bc
suu3 <ahob789@ewhain.net>
2022-06-16 12:33:21 +0900

    [FIX] login

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,9 @@
 import { client } from "./auth";
 
 export async function getUserInfo() {
+  console.log("response");
+
   const response = await client.get("/user/userInfo");
+  console.log(response);
   return response;
 }
